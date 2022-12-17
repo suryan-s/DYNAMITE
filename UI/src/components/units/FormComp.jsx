5a352364091e24887d3d3ca55573142b0eba9910
Arjun <arjun8107@gmail.com>
2022-12-18 00:20:26 +0530

    form

--- UI/src/components/units/FormComp.jsx
+++ UI/src/components/units/FormComp.jsx
@@ -1,4 +1,63 @@
-import { Box, Input } from "@chakra-ui/react";
+import { useState, useEffect, useRef } from "react";
+import {
+  Box,
+  Input,
+  Select,
+  FormControl,
+  FormLabel,
+  Button,
+} from "@chakra-ui/react";
+
+const fields = {
+  heart: [
+    "Age",
+    "Sex",
+    "Chest_pain_type",
+    "BP",
+    "Cholesterol",
+    "FBS_over_120",
+    "EK_results",
+    "Max_HR",
+    "Exercise_angina",
+    "ST_depression",
+    "Slope_of_ST",
+    "Number_of_vessels_fluro",
+    "Thallium",
+  ],
+};
+
 export default function FormComp() {
-  return <form></form>;
+  const [formItems, setFormItems] = useState([]);
+  const formElement = useRef();
+  function subForm() {
+    const data = new URLSearchParams(new FormData(formElement));
+    fetch(url, {
+      method: "post",
+      body: data,
+    }).then((res) => {
+      console.log(res.json());
+    });
+  }
+  return (
+    <>
+      <Select
+        placeholder="select option"
+        onChange={(e) => {
+          setFormItems(fields[e.target.value]);
+          console.log(e.target.value);
+        }}
+      >
+        <option value="heart">Heart</option>
+      </Select>
+      <form ref={formElement}>
+        {formItems.map((item, idx) => (
+          <FormControl key={idx} px="1rem">
+            <FormLabel>{item}</FormLabel>
+            <Input type="text" />
+          </FormControl>
+        ))}
+      </form>
+      {formItems[1] && <Button onClick={subForm}>Submit</Button>}
+    </>
+  );
 }
