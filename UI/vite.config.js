baf3328f02fbd76f59e144d9b38717d8ae3adaa8
Arjun <arjun8107@gmail.com>
2022-12-18 00:32:38 +0530

    added compiled

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -4,6 +4,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    outDir:"built",
+  },
   plugins: [react(),
     VitePWA({ registerType: 'autoUpdate',
     includeAssets: ['favicon.svg', 'favicon.png'],
